8dd6ba32615fd3406555b2541b85ba37a7979fc3
Carlos Aguirre <carlosanibal815@gmail.com>
2025-10-25 21:57:45 -0300

    Instalando app y pillow

--- Proyecto1/Proyecto1/settings.py
+++ Proyecto1/Proyecto1/settings.py
@@ -39,7 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myapp',
     'widget_tweaks',
+    'accounts',
 ]
+
+LOGIN_REDIRECT_URL = 'accounts:perfil'
+LOGOUT_REDIRECT_URL = 'accounts:login'
+LOGIN_URL = 'accounts:login'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
